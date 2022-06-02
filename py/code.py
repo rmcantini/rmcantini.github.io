ae64e4e36edbc545b5332d7a56b86cfeef1ffc03
rmcantini <rmcantini@gmail.com>
2022-06-02 15:16:51 -0300

    test code plus real code

---
+++ py/code.py
@@ -0,0 +1,22 @@
+'''Timesheet calc'''
+from datetime import datetime
+
+
+TOTAL_TIME = '08:00'
+
+
+worked_time = input(
+    'Digite o total de horas registradas em tasks do dia (00:00): ')
+# print(worked_time)
+
+FORMAT_H = '%H:%M'
+
+time_solo = datetime.strptime(TOTAL_TIME, FORMAT_H) - \
+    datetime.strptime(worked_time, FORMAT_H)
+time_final = time_solo / 2
+
+
+answer = (f'Tempo de espera total {time_solo}. \nRegistre ambos, \
+Super e Eletro, com {time_final} cada.')
+
+print('Timesheet', answer)
